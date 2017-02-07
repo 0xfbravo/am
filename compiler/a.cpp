ab5183b5c6d713cc5d1da58771882aa3d4187862
Fellipe Bravo Ribeiro Pimentel <insidemybrain@Fellipes-Macbook.local>
2017-02-07 01:00:02 -0200

    Adicionado README

---
+++ compiler/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	switch(1){
+		case 1:
+		cout <<"ou" << endl;
+		case 10:
+		cout << "ei" << endl;
+		default:
+		cout << "aaaa" << endl;
+	}
+}
